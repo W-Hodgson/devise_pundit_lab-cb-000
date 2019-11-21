9c5c4e216592118bfb134c5ca42ffc440d8082db
Will Hodgson <will.hodgson@current-rms.com>
2019-11-21 13:03:53 +0000

    Done.

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -7,14 +7,7 @@ feature 'Sign out', :devise do
   # Scenario: User signs out successfully
   #   Given I am signed in
   #   When I sign out
-  #   Then I see a signed out message
-  scenario 'user signs out successfully' do
-    user = FactoryBot.create(:user)
-    signin(user.email, user.password)
-    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
-    click_link 'Sign out'
-    expect(page).to have_content I18n.t 'devise.sessions.signed_out'
-  end
+
 
 end
 
--- spec/features/visitors/about_page_spec.rb
+++ spec/features/visitors/about_page_spec.rb
@@ -8,9 +8,6 @@ feature 'About page' do
   #   Given I am a visitor
   #   When I visit the 'about' page
   #   Then I see "About the Website"
-  scenario 'Visit the about page' do
-    visit 'pages/about'
-    expect(page).to have_content 'About the Website'
-  end
+
 
 end
